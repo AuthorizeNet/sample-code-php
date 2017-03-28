39e16895e15ccc164e133682b81542ab89c08283
Aaron Wright <aaron@infocreations.com>
2017-03-28 10:38:14 -0700

    Match sample in MobileInappTransactions

--- PaymentTransactions/create-an-accept-payment-transaction.php
+++ PaymentTransactions/create-an-accept-payment-transaction.php
@@ -7,21 +7,26 @@
   define("AUTHORIZENET_LOG_FILE", "phplog");
 
   function createAnAcceptPaymentTransaction($amount){
-    // Common setup for API credentials
+    // Create a merchantAuthenticationType object with authentication details
+    // retrieved from the constants file
     $merchantAuthentication = new AnetAPI\MerchantAuthenticationType();
     $merchantAuthentication->setName(\SampleCode\Constants::MERCHANT_LOGIN_ID);
     $merchantAuthentication->setTransactionKey(\SampleCode\Constants::MERCHANT_TRANSACTION_KEY);
     $refId = 'ref' . time();
 
     // Create the payment object for a payment nonce
-    $paymentNonce = new AnetAPI\OpaqueDataType();
-    $paymentNonce->setDataDescriptor("COMMON.ACCEPT.INAPP.PAYMENT");
-    $paymentNonce->setDataValue("119eyJjb2RlIjoiNTBfMl8wNjAwMDUyN0JEODE4RjQxOUEyRjhGQkIxMkY0MzdGQjAxQUIwRTY2NjhFNEFCN0VENzE4NTUwMjlGRUU0M0JFMENERUIwQzM2M0ExOUEwMDAzNzlGRDNFMjBCODJEMDFCQjkyNEJDIiwidG9rZW4iOiI5NDkwMjMyMTAyOTQwOTk5NDA0NjAzIiwidiI6IjEuMSJ9");
+    $opaqueData = new AnetAPI\OpaqueDataType();
+    $opaqueData->setDataDescriptor("COMMON.ACCEPT.INAPP.PAYMENT");
+    $opaqueData->setDataValue("119eyJjb2RlIjoiNTBfMl8wNjAwMDUyN0JEODE4RjQxOUEyRjhGQkIxMkY0MzdGQjAxQUIwRTY2NjhFNEFCN0VENzE4NTUwMjlGRUU0M0JFMENERUIwQzM2M0ExOUEwMDAzNzlGRDNFMjBCODJEMDFCQjkyNEJDIiwidG9rZW4iOiI5NDkwMjMyMTAyOTQwOTk5NDA0NjAzIiwidiI6IjEuMSJ9");
+    
+    // Add the payment data to a paymentType object
     $paymentOne = new AnetAPI\PaymentType();
-    $paymentOne->setOpaqueData($paymentNonce);
+    $paymentOne->setOpaqueData($opaqueData);
 
+    // Create order information
     $order = new AnetAPI\OrderType();
-    $order->setDescription("New Item");
+    $order->invoiceNumber("10101");
+    $order->setDescription("Golf Shirts");
 
     // Set the customer's Bill To address
     $customerAddress = new AnetAPI\CustomerAddressType();
@@ -45,7 +50,7 @@
     $duplicateWindowSetting->setSettingName("duplicateWindow");
     $duplicateWindowSetting->setSettingValue("600");
 
-    // Create a TransactionRequestType object
+    // Create a transactionRequestType object and add the previous objects to it
     $transactionRequestType = new AnetAPI\TransactionRequestType();
     $transactionRequestType->setTransactionType( "authCaptureTransaction"); 
     $transactionRequestType->setAmount($amount);
@@ -55,19 +60,24 @@
     $transactionRequestType->setCustomer($customerData);
     $transactionRequestType->addToTransactionSettings($duplicateWindowSetting);
 
+    // Assemble the complete transaction request
     $request = new AnetAPI\CreateTransactionRequest();
     $request->setMerchantAuthentication($merchantAuthentication);
     $request->setRefId( $refId);
     $request->setTransactionRequest( $transactionRequestType);
 
+    // Create the controller and get response
     $controller = new AnetController\CreateTransactionController($request);
     $response = $controller->executeWithApiResponse( \net\authorize\api\constants\ANetEnvironment::SANDBOX);
     
 
     if ($response != null)
     {
+      // Check to see if the API request was successfully received and acted upon
       if($response->getMessages()->getResultCode() == \SampleCode\Constants::RESPONSE_OK)
       {
+        # Since the API request was successful, look for a transaction response
+        # and parse it to display the results of authorizing the card
         $tresponse = $response->getTransactionResponse();
         
         if ($tresponse != null && $tresponse->getMessages() != null)   
@@ -88,6 +98,7 @@
           }
         }
       }
+      // Or, print errors if the API request wasn't successful
       else
       {
         echo "Transaction Failed \n";

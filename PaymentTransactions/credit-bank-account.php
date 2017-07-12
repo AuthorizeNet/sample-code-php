afb1a70fc04b7aafae57572f877a192d253faa46
adavidw <aaron@infocreations.com>
2017-07-12 01:32:06 -0600

    changes to help tests pass

--- PaymentTransactions/credit-bank-account.php
+++ PaymentTransactions/credit-bank-account.php
@@ -18,81 +18,71 @@ function creditBankAccount($amount)
 
     // Create the payment data for a Bank Account
     $bankAccount = new AnetAPI\BankAccountType();
-    $bankAccount->setRoutingNumber('125000024');
+    $bankAccount->setAccountType('checking');
+    // $bankAccount->setEcheckType('WEB');
+    $bankAccount->setRoutingNumber('121042882');
     $bankAccount->setAccountNumber('12345678');
-    $bankAccount->setNameOnAccount('Jane Doe');
+    $bankAccount->setNameOnAccount('John Doe');
+    $bankAccount->setBankName('Bank of the Earth');
     
     $paymentBank= new AnetAPI\PaymentType();
     $paymentBank->setBankAccount($bankAccount);
 
-  // Order info
+    // Order info
     $order = new AnetAPI\OrderType();
     $order->setInvoiceNumber("101");
     $order->setDescription("Golf Shirts");
 
-      //create a debit card Bank transaction
+      //create a bank credit transaction
     
     $transactionRequestType = new AnetAPI\TransactionRequestType();
-    $transactionRequestType->setTransactionType( "refundTransaction"); 
+    $transactionRequestType->setTransactionType("refundTransaction");
     $transactionRequestType->setAmount($amount);
     $transactionRequestType->setPayment($paymentBank);
     $transactionRequestType->setOrder($order);
     $request = new AnetAPI\CreateTransactionRequest();
     $request->setMerchantAuthentication($merchantAuthentication);
-    $request->setRefId( $refId);
-    $request->setTransactionRequest( $transactionRequestType);
+    $request->setRefId($refId);
+    $request->setTransactionRequest($transactionRequestType);
     $controller = new AnetController\CreateTransactionController($request);
-    $response = $controller->executeWithApiResponse( \net\authorize\api\constants\ANetEnvironment::SANDBOX);
+    $response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX);
 
-
-    if ($response != null)
-    {
-      if($response->getMessages()->getResultCode() == \SampleCode\Constants::RESPONSE_OK)
-      {
-        $tresponse = $response->getTransactionResponse();
+    if ($response != null) {
+        if ($response->getMessages()->getResultCode() == \SampleCode\Constants::RESPONSE_OK) {
+            $tresponse = $response->getTransactionResponse();
         
-	      if ($tresponse != null && $tresponse->getMessages() != null)   
-        {
-          echo " Transaction Response code : " . $tresponse->getResponseCode() . "\n";
-          echo  "Credit Bank Account APPROVED  :" . "\n";
-          echo  "Credit Bank Account AUTH CODE : " . $tresponse->getAuthCode() . "\n";
-          echo  "Credit Bank Account TRANS ID  : " . $tresponse->getTransId() . "\n";
-          echo " Code : " . $tresponse->getMessages()[0]->getCode() . "\n"; 
-	        echo " Description : " . $tresponse->getMessages()[0]->getDescription() . "\n";
+            if ($tresponse != null && $tresponse->getMessages() != null) {
+                echo " Transaction Response code : " . $tresponse->getResponseCode() . "\n";
+                echo  "Credit Bank Account APPROVED  :" . "\n";
+                echo  "Credit Bank Account AUTH CODE : " . $tresponse->getAuthCode() . "\n";
+                echo  "Credit Bank Account TRANS ID  : " . $tresponse->getTransId() . "\n";
+                echo " Code : " . $tresponse->getMessages()[0]->getCode() . "\n";
+                echo " Description : " . $tresponse->getMessages()[0]->getDescription() . "\n";
+            } else {
+                echo "Transaction Failed \n";
+                if ($tresponse->getErrors() != null) {
+                    echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+                    echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+                }
+            }
+        } else {
+            echo "Transaction Failed \n";
+            $tresponse = $response->getTransactionResponse();
+            if ($tresponse != null && $tresponse->getErrors() != null) {
+                echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+                echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+            } else {
+                echo " Error code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
+                echo " Error message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
+            }
         }
-        else
-        {
-          echo "Transaction Failed \n";
-          if($tresponse->getErrors() != null)
-          {
-            echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-            echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";            
-          }
-        }
-      }
-      else
-      {
-        echo "Transaction Failed \n";
-        $tresponse = $response->getTransactionResponse();
-        if($tresponse != null && $tresponse->getErrors() != null)
-        {
-          echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-          echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";                      
-        }
-        else
-        {
-          echo " Error code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
-          echo " Error message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
-        }
-      }      
-    }
-    else
-    {
-      echo  "No response returned \n";
+    } else {
+        echo  "No response returned \n";
     }
 
     return $response;
-  }
-  if(!defined('DONT_RUN_SAMPLES'))
-    creditBankAccount(12.23);
-?>
+}
+
+if (!defined('DONT_RUN_SAMPLES')) {
+    creditBankAccount(5.29);
+}
--- PaymentTransactions/debit-bank-account.php
+++ PaymentTransactions/debit-bank-account.php
@@ -19,7 +19,7 @@ function debitBankAccount($amount)
     // Create the payment data for a Bank Account
     $bankAccount = new AnetAPI\BankAccountType();
     $bankAccount->setAccountType('checking');
-    $bankAccount->setEcheckType('WEB');
+    // $bankAccount->setEcheckType('WEB');
     $bankAccount->setRoutingNumber('121042882');
     $bankAccount->setAccountNumber('12345678');
     $bankAccount->setNameOnAccount('John Doe');
@@ -28,68 +28,61 @@ function debitBankAccount($amount)
     $paymentBank= new AnetAPI\PaymentType();
     $paymentBank->setBankAccount($bankAccount);
 
+    // Order info
+    $order = new AnetAPI\OrderType();
+    $order->setInvoiceNumber("101");
+    $order->setDescription("Golf Shirts");
 
-    //create a debit card Bank transaction
+    //create a bank debit transaction
     
     $transactionRequestType = new AnetAPI\TransactionRequestType();
-    $transactionRequestType->setTransactionType( "authCaptureTransaction"); 
+    $transactionRequestType->setTransactionType("authCaptureTransaction");
     $transactionRequestType->setAmount($amount);
     $transactionRequestType->setPayment($paymentBank);
-
+    $transactionRequestType->setOrder($order);
     $request = new AnetAPI\CreateTransactionRequest();
     $request->setMerchantAuthentication($merchantAuthentication);
-    $request->setRefId( $refId);
-    $request->setTransactionRequest( $transactionRequestType);
+    $request->setRefId($refId);
+    $request->setTransactionRequest($transactionRequestType);
     $controller = new AnetController\CreateTransactionController($request);
-    $response = $controller->executeWithApiResponse( \net\authorize\api\constants\ANetEnvironment::SANDBOX);
+    $response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX);
 
-    if ($response != null)
-    {
-      if($response->getMessages()->getResultCode() == \SampleCode\Constants::RESPONSE_OK)
-      {
-        $tresponse = $response->getTransactionResponse();
+    if ($response != null) {
+        if ($response->getMessages()->getResultCode() == \SampleCode\Constants::RESPONSE_OK) {
+            $tresponse = $response->getTransactionResponse();
         
-	      if ($tresponse != null && $tresponse->getMessages() != null)   
-        {
-          echo " Transaction Response code : " . $tresponse->getResponseCode() . "\n";
-          echo  "Debit Bank Account APPROVED  :" . "\n";
-          echo " Debit Bank Account AUTH CODE : " . $tresponse->getAuthCode() . "\n";
-          echo " Debit Bank Account TRANS ID  : " . $tresponse->getTransId() . "\n";
-          echo " Code : " . $tresponse->getMessages()[0]->getCode() . "\n"; 
-	        echo " Description : " . $tresponse->getMessages()[0]->getDescription() . "\n";
+            if ($tresponse != null && $tresponse->getMessages() != null) {
+                echo " Transaction Response code : " . $tresponse->getResponseCode() . "\n";
+                echo " Debit Bank Account APPROVED  :" . "\n";
+                echo " Debit Bank Account AUTH CODE : " . $tresponse->getAuthCode() . "\n";
+                echo " Debit Bank Account TRANS ID  : " . $tresponse->getTransId() . "\n";
+                echo " Code : " . $tresponse->getMessages()[0]->getCode() . "\n";
+                echo " Description : " . $tresponse->getMessages()[0]->getDescription() . "\n";
+            } else {
+                echo "Transaction Failed \n";
+                if ($tresponse->getErrors() != null) {
+                    echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+                    echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+                }
+            }
+        } else {
+            echo "Transaction Failed \n";
+            $tresponse = $response->getTransactionResponse();
+            if ($tresponse != null && $tresponse->getErrors() != null) {
+                echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+                echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+            } else {
+                echo " Error code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
+                echo " Error message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
+            }
         }
-        else
-        {
-          echo "Transaction Failed \n";
-          if($tresponse->getErrors() != null)
-          {
-            echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-            echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";            
-          }
-        }
-      }
-      else
-      {
-        echo "Transaction Failed \n";
-        $tresponse = $response->getTransactionResponse();
-        if($tresponse != null && $tresponse->getErrors() != null)
-        {
-          echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-          echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";                      
-        }
-        else
-        {
-          echo " Error code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
-          echo " Error message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
-        }
-      }      
-    }
-    else
-    {
-      echo  "No response returned \n";
+    } else {
+        echo  "No response returned \n";
     }
 
     return $response;
-  }
-  if(!defined('DONT_RUN_SAMPLES'))
+}
+
+if (!defined('DONT_RUN_SAMPLES')) {
     debitBankAccount(5.29);
+}

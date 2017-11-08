f513ad21675a88a2af547688f8ca37010e800652
adavidw <aaron@infocreations.com>
2017-11-08 12:10:12 -0700

    Match the names as used in the API Reference

--- PayPalExpressCheckout/authorization-and-capture-continued.php
+++ PayPalExpressCheckout/authorization-and-capture-continued.php
@@ -1,7 +1,7 @@
 <?php
-	require 'vendor/autoload.php';
-	use net\authorize\api\contract\v1 as AnetAPI;
-	use net\authorize\api\controller as AnetController;
+    require 'vendor/autoload.php';
+    use net\authorize\api\contract\v1 as AnetAPI;
+    use net\authorize\api\controller as AnetController;
 
     define("AUTHORIZENET_LOG_FILE", "phplog");
 
@@ -20,71 +20,58 @@ function payPalAuthorizeCaptureContinued($refTransId, $payerID)
     $payPalType=new AnetAPI\PayPalType();
     $payPalType->setPayerID($payerID);
 
-		$paymentOne = new AnetAPI\PaymentType();
-		$paymentOne->setPayPal($payPalType);
+    $paymentOne = new AnetAPI\PaymentType();
+    $paymentOne->setPayPal($payPalType);
 
-		// Create an authorize and capture continue transaction
-		$transactionRequestType = new AnetAPI\TransactionRequestType();
-		$transactionRequestType->setTransactionType( "authCaptureContinueTransaction");
-		$transactionRequestType->setPayment($paymentOne);
-		$transactionRequestType->setRefTransId($refTransId);
+    // Create an authorize and capture continued transaction
+    $transactionRequestType = new AnetAPI\TransactionRequestType();
+    $transactionRequestType->setTransactionType("authCaptureContinueTransaction");
+    $transactionRequestType->setPayment($paymentOne);
+    $transactionRequestType->setRefTransId($refTransId);
 
-		$request = new AnetAPI\CreateTransactionRequest();
-		$request->setMerchantAuthentication($merchantAuthentication);
-		$request->setTransactionRequest( $transactionRequestType);
-		$controller = new AnetController\CreateTransactionController($request);
-		$response = $controller->executeWithApiResponse( \net\authorize\api\constants\ANetEnvironment::SANDBOX);
+    $request = new AnetAPI\CreateTransactionRequest();
+    $request->setMerchantAuthentication($merchantAuthentication);
+    $request->setTransactionRequest($transactionRequestType);
+    $controller = new AnetController\CreateTransactionController($request);
+    $response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX);
 
-	    if ($response != null)
-	    {
-	      if($response->getMessages()->getResultCode() == \SampleCode\Constants::RESPONSE_OK)
-	      {
-	        $tresponse = $response->getTransactionResponse();
-	        
-	        if ($tresponse != null && $tresponse->getMessages() != null)   
-	        {
-				echo "Transaction Response...\n";
-          		echo " Transaction Response code : " . $tresponse->getResponseCode() . "\n";
-				//Valid response codes: 1=Approved, 2=Declined, 3=Error, 5=Need Payer Consent
-				echo "Secure acceptance URL: ".$tresponse->getSecureAcceptance()->getSecureAcceptanceUrl()."\n";
-				echo "Transaction ID: ".$tresponse->getTransId()."\n";
-	          	echo " Code : " . $tresponse->getMessages()[0]->getCode() . "\n"; 
-	          	echo " Description : " . $tresponse->getMessages()[0]->getDescription() . "\n";
-	        }
-	        else
-	        {
-	          echo "Transaction Failed \n";
-	          if($tresponse->getErrors() != null)
-	          {
-	            echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-	            echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";            
-	          }
-	        }
-	      }
-	      else
-	      {
-	        echo "Transaction Failed \n";
-	        $tresponse = $response->getTransactionResponse();
-	        if($tresponse != null && $tresponse->getErrors() != null)
-	        {
-	          echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-	          echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";                      
-	        }
-	        else
-	        {
-	          echo " Error code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
-	          echo " Error message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
-	        }
-	      }      
-	    }
-	    else
-	    {
-	      echo  "No response returned \n";
-	    }
+    if ($response != null) {
+        if ($response->getMessages()->getResultCode() == \SampleCode\Constants::RESPONSE_OK) {
+            $tresponse = $response->getTransactionResponse();
+          
+            if ($tresponse != null && $tresponse->getMessages() != null) {
+                echo "Transaction Response...\n";
+                echo " Transaction Response code : " . $tresponse->getResponseCode() . "\n";
+                //Valid response codes: 1=Approved, 2=Declined, 3=Error, 5=Need Payer Consent
+                echo "Secure acceptance URL: ".$tresponse->getSecureAcceptance()->getSecureAcceptanceUrl()."\n";
+                echo "Transaction ID: ".$tresponse->getTransId()."\n";
+                echo " Code : " . $tresponse->getMessages()[0]->getCode() . "\n";
+                echo " Description : " . $tresponse->getMessages()[0]->getDescription() . "\n";
+            } else {
+                echo "Transaction Failed \n";
+                if ($tresponse->getErrors() != null) {
+                    echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+                    echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+                }
+            }
+        } else {
+            echo "Transaction Failed \n";
+            $tresponse = $response->getTransactionResponse();
+            if ($tresponse != null && $tresponse->getErrors() != null) {
+                echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+                echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+            } else {
+                echo " Error code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
+                echo " Error message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
+            }
+        }
+    } else {
+        echo  "No response returned \n";
+    }
 
-		return $response;
-	}
+    return $response;
+}
 
-  	if(!defined('DONT_RUN_SAMPLES'))
-    	payPalAuthorizeCaptureContinued("2241708986","6ZSCSYG33VP8Q");
-?>
+if (!defined('DONT_RUN_SAMPLES')) {
+    payPalAuthorizeCaptureContinued("2241708986", "6ZSCSYG33VP8Q");
+}
--- PayPalExpressCheckout/authorization-only-continued.php
+++ PayPalExpressCheckout/authorization-only-continued.php
@@ -1,15 +1,15 @@
 <?php
-  require 'vendor/autoload.php';
+    require 'vendor/autoload.php';
 
-  use net\authorize\api\contract\v1 as AnetAPI;
-  use net\authorize\api\controller as AnetController;
+    use net\authorize\api\contract\v1 as AnetAPI;
+    use net\authorize\api\controller as AnetController;
 
-  define("AUTHORIZENET_LOG_FILE", "phplog");
+    define("AUTHORIZENET_LOG_FILE", "phplog");
 
-function payPalAuthorizeOnlyContinue($transactionId, $payerId)
+function payPalAuthorizeOnlyContinued($transactionId, $payerId)
 {
 
- echo "PayPal Authorize Only Continue Transaction\n";
+    echo "PayPal Authorize Only Continued Transaction\n";
     
     /* Create a merchantAuthenticationType object with authentication details
        retrieved from the constants file */
@@ -24,7 +24,7 @@ function payPalAuthorizeOnlyContinue($transactionId, $payerId)
     $payPalType=new AnetAPI\PayPalType();
     $payPalType->setPayerID($payerID);
 
-    $paypal_type->setSuccessUrl("http://www.merchanteCommerceSite.com/Success/TC25262");  
+    $paypal_type->setSuccessUrl("http://www.merchanteCommerceSite.com/Success/TC25262");
     $paypal_type->setCancelUrl("http://www.merchanteCommerceSite.com/Success/TC25262");
     
     $payment_type = new AnetAPI\PaymentType();
@@ -32,68 +32,54 @@ function payPalAuthorizeOnlyContinue($transactionId, $payerId)
 
     //create a transaction
     $transactionRequestType = new AnetAPI\TransactionRequestType();
-    $transactionRequestType->setTransactionType( "authOnlyContinueTransaction"); 
+    $transactionRequestType->setTransactionType("authOnlyContinueTransaction");
     $transactionRequestType->setRefTransId($transactionId);
     $transactionRequestType->setAmount(125.34);
     $transactionRequestType->setPayment($payment_type);
 
     $request = new AnetAPI\CreateTransactionRequest();
     $request->setMerchantAuthentication($merchantAuthentication);
-    $request->setRefId( $refId);
-    $request->setTransactionRequest( $transactionRequestType);
+    $request->setRefId($refId);
+    $request->setTransactionRequest($transactionRequestType);
 
     $controller = new AnetController\CreateTransactionController($request);
 
-    $response = $controller->executeWithApiResponse( \net\authorize\api\constants\ANetEnvironment::SANDBOX);
+    $response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX);
 
-    if ($response != null)
-    {
-      if($response->getMessages()->getResultCode() == \SampleCode\Constants::RESPONSE_OK)
-      {
-        $tresponse = $response->getTransactionResponse();
+    if ($response != null) {
+        if ($response->getMessages()->getResultCode() == \SampleCode\Constants::RESPONSE_OK) {
+            $tresponse = $response->getTransactionResponse();
         
-	      if ($tresponse != null && $tresponse->getMessages() != null)   
-        {
-          echo " Transaction Response code : " . $tresponse->getResponseCode() . "\n";
-          echo "TRANS ID  : " . $tresponse->getTransId() . "\n";
-          echo "Payer ID : " . $tresponse->getSecureAcceptance()->getPayerID();      
-          echo "Description : " . $tresponse->getMessages()[0]->getDescription() . "\n";
+            if ($tresponse != null && $tresponse->getMessages() != null) {
+                echo " Transaction Response code : " . $tresponse->getResponseCode() . "\n";
+                echo "TRANS ID  : " . $tresponse->getTransId() . "\n";
+                echo "Payer ID : " . $tresponse->getSecureAcceptance()->getPayerID();
+                echo "Description : " . $tresponse->getMessages()[0]->getDescription() . "\n";
+            } else {
+                echo "Transaction Failed \n";
+                if ($tresponse->getErrors() != null) {
+                    echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+                    echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+                }
+            }
+        } else {
+            echo "Transaction Failed \n";
+            $tresponse = $response->getTransactionResponse();
+            if ($tresponse != null && $tresponse->getErrors() != null) {
+                echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+                echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+            } else {
+                echo " Error code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
+                echo " Error message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
+            }
         }
-        else
-        {
-          echo "Transaction Failed \n";
-          if($tresponse->getErrors() != null)
-          {
-            echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-            echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";            
-          }
-        }
-      }
-      else
-      {
-        echo "Transaction Failed \n";
-        $tresponse = $response->getTransactionResponse();
-        if($tresponse != null && $tresponse->getErrors() != null)
-        {
-          echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-          echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";                      
-        }
-        else
-        {
-          echo " Error code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
-          echo " Error message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
-        }
-      }      
-    }
-    else
-    {
-      echo  "No response returned \n";
+    } else {
+        echo  "No response returned \n";
     }
 
     return $response;
-  }
+}
   
-  if(!defined('DONT_RUN_SAMPLES'))
-      payPalAuthorizeOnlyContinue("2241711631", "JJLRRB29QC7RU");
-
-?>
+if (!defined('DONT_RUN_SAMPLES')) {
+    payPalAuthorizeOnlyContinued("2241711631", "JJLRRB29QC7RU");
+}

e15a77bdb6954c6626a745212a33c9f84042bc0b
khaaldrogo <35258595+khaaldrogo@users.noreply.github.com>
2018-02-22 07:44:06 +0000

    Added includeTransactions and formatting changes

--- RecurringBilling/get-subscription.php
+++ RecurringBilling/get-subscription.php
@@ -21,7 +21,8 @@
     $request->setMerchantAuthentication($merchantAuthentication);
     $request->setRefId($refId);
     $request->setSubscriptionId($subscriptionId);
-		
+    $request->setIncludeTransactions(true);
+	    
     // Controller
     $controller = new AnetController\ARBGetSubscriptionController($request);
 		

9f8363a6fd72f6248bd9017339563c49926a92b3
Kaushik Kumar Mahato <kikmak42@null.net>
2018-04-06 16:58:38 +0530

    Update debit-bank-account.php

--- PaymentTransactions/debit-bank-account.php
+++ PaymentTransactions/debit-bank-account.php
@@ -21,7 +21,7 @@ function debitBankAccount($amount)
     $bankAccount->setAccountType('checking');
     // see eCheck documentation for proper echeck type to use for each situation
     $bankAccount->setEcheckType('WEB');
-    $bankAccount->setRoutingNumber('122105155');
+    $bankAccount->setRoutingNumber('125000105');
     $bankAccount->setAccountNumber('1234567890');
     $bankAccount->setNameOnAccount('John Doe');
     $bankAccount->setBankName('Wells Fargo Bank NA');

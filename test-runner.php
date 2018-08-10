8dd58e3cde60b3630945b2da415a021b8ca049f2
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2018-08-10 17:04:15 +0530

    + Fixing test-runner.php

--- test-runner.php
+++ test-runner.php
@@ -14,6 +14,7 @@ if (substr($dirPath, -1) != "/") {
 }
 
 $directories = array(
+            'AcceptSuite/',
             'CustomerProfiles/',
             'RecurringBilling/',
             'PayPalExpressCheckout/',

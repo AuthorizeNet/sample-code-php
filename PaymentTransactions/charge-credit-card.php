f178458dd070aee45d22eda2b48ac82d8105b54a
Kevin Matsuo <kmatsuo@ards-mbp-2.attlocal.net>
2020-09-17 17:22:25 -0500

    updated filepath for require on line 3 for SPL autoload since we are not running Composer

--- PaymentTransactions/charge-credit-card.php
+++ PaymentTransactions/charge-credit-card.php
@@ -1,5 +1,6 @@
 <?php
-  require 'vendor/autoload.php';
+//   require 'vendor/autoload.php';
+  require '../../../autoload.php';
   require_once 'constants/SampleCodeConstants.php';
   use net\authorize\api\contract\v1 as AnetAPI;
   use net\authorize\api\controller as AnetController;
